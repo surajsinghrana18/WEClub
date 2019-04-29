6e751702f1523e9e4cca106854748d46fa21ca98
JagritGupta <43613933+JagritGupta@users.noreply.github.com>
2019-04-29 15:06:20 +0530

    Add files via upload

---
+++ Main2Activity.java
@@ -0,0 +1,17 @@
+package io.github.srmwe.weclub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+}
